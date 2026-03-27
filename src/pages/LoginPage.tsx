c24112dd81229a18dfea0cca66ddef2e402229a1
ny-tea-t <tea.timotijevic@edu.tbz.ch>
2026-03-27 13:45:25 +0100

    update login to email and password

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log('Login:', { email, password });
+    alert('Login versucht');
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Passwort"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br /><br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
